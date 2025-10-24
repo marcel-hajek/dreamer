9d30541583b73ec600f6993004a719cd85381585
marcel <marcel.hajek@gmx.de>
2025-10-25 01:05:52 +0200

    ...

--- include/dreamer/lexer/Lexer.h
+++ include/dreamer/lexer/Lexer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <expected>
+#include <optional>
 #include <vector>
 
 #include "dreamer/token/Token.h"
@@ -10,8 +11,15 @@ public:
   enum class ErrorKind {
     ForbiddenSymbol,
   };
-  [[nodiscard]] static auto run(std::string &&source_core) noexcept -> std::expected< std::vector<token::Token>, ErrorKind>;
+  [[nodiscard]] static auto run(std::vector<std::string> &&source_core) noexcept
+      -> std::expected<std::vector<token::Token>, ErrorKind>;
 
 private:
-  [[nodiscard]] static auto search(const char target, const std::initializer_list<char> &list) noexcept -> bool;
+  [[nodiscard]] static auto
+  split_tokens(std::vector<std::string> &&source_core) noexcept
+      -> std::expected<std::vector<token::Token>, ErrorKind>;
+
+  [[nodiscard]] static auto
+  search(const char target, const std::initializer_list<char> &list) noexcept
+      -> std::optional<char>;
 };
--- lib/lexer/Lexer.cpp
+++ lib/lexer/Lexer.cpp
@@ -1,24 +1,42 @@
 #include "dreamer/lexer/Lexer.h"
+#include "dreamer/token/Token.h"
 
 #include <expected>
 #include <initializer_list>
+#include <optional>
 #include <vector>
 
-auto Lexer::run(std::string &&source_core) noexcept -> std::expected< std::vector<token::Token>, ErrorKind> {
-  const std::initializer_list<char> FORBIDDEN_CHARS {'\t'};
+auto Lexer::run(std::vector<std::string> &&source_core) noexcept
+    -> std::expected<std::vector<token::Token>, ErrorKind> {
+  const std::initializer_list<char> forbidden_chars{
+      '\t',
+  },
+      included_chars{
+          '(',
+          ')',
+          ':',
+          '=',
+      };
 
   std::vector<token::Token> ret{};
-
+  std::string last_word{};
   for (const char source_code_char : source_core) {
-    if (search(source_code_char, FORBIDDEN_CHARS)) [[unlikely]] {
-      return std::unexpected {ErrorKind::ForbiddenSymbol};
+    if (const auto &&_ = search(source_code_char, forbidden_chars))
+        [[unlikely]] {
+      return std::unexpected{ErrorKind::ForbiddenSymbol};
+    } else if (const auto &&opt_char = search(source_code_char, included_chars))
+        [[likely]] {
+
+      last_word.push_back(opt_char.value());
     }
   }
 
   return ret;
 }
 
-auto Lexer::search(const char target, const std::initializer_list<char> &list) noexcept -> bool {
+auto Lexer::search(const char target,
+                   const std::initializer_list<char> &list) noexcept
+    -> std::optional<char> {
   for (auto &&list_char : list) {
     if (target == list_char) {
       return true;
